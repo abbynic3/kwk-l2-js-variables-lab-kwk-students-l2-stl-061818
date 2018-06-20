b7203425e35d24929e0fb984a9cfa2897a445789
Abby Lortz <abilenelortz03@gmail.com>
2018-06-20 20:23:32 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,16 +1,18 @@
 const expect = chai.expect;
+const companyName = 'Scuber';
 
+ 
 describe('index.js', function () {
   describe('companyName', function () {
-    it('is set as Scuber', function () {
+    it('is declared as Scuber', function () {
       expect(companyName).to.equal('Scuber');
     });
 
     it('raises error if the companyName is changed', function () {
-      expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
+      expect(function () { companyName = 'Scuber' }).to.throw(TypeError);
     });
   });
-
+let mostProfitableNeighborhood = 'Chelsea';
   describe('mostProfitableNeighborhood', function () {
     it('is declared as equal to Chelsea', function () {
       expect(mostProfitableNeighborhood).to.equal('Chelsea');
@@ -20,7 +22,7 @@ describe('index.js', function () {
       expect(function () { mostProfitableNeighborhood = 'Upper West Side' }).to.not.throw(TypeError);
     });
   });
-
+let companyCeo = 'Susan Smith';
   describe('companyCeo', function () {
     it('is declared as equal to Susan Smith', function () {
       expect(companyCeo).to.equal('Susan Smith');
